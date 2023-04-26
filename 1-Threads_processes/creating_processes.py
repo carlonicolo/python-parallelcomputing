bbe6339c6d49e4b512323465cf0df627666e51a2
Carlo <carlo.nicolo@hotmail.com>
2023-04-26 20:08:42 +0200

    Processes example

---
+++ 1-Threads_processes/creating_processes.py
@@ -0,0 +1,20 @@
+import multiprocessing
+import time
+from threading import Thread
+from multiprocessing import Process
+
+def do_work():
+    print("Starting work")
+    i = 0
+    for _ in range(20000000):
+        i += 1
+    #time.sleep(1)
+    print("Finished work")
+   
+if __name__ == '__main__':
+    multiprocessing.set_start_method('spawn')
+ 
+    for _ in range(5):
+        p = Process(target=do_work, args=())
+        p.start()
+    
